b1c171001ca8fc38d7f5e60afffb1df47f0eb44d
kahtab1324 <k.s.fayyad.dev@gmail.com>
2022-12-21 20:46:23 +0200

    update

---
+++ js1/js-basic/basic.js
@@ -0,0 +1,47 @@
+// here just note
+// ------------------------------math operations ----------------------------------------
+
+// Addition
+10 + 5;
+
+// ------------ subtraction----------------------
+230 - 10;
+
+// --------------------multipaication --------------------\
+4 * 5;
+
+// division
+
+400 / 25;
+
+// modulo
+27 % 2; //1
+// i will ask this quastion how many 2 in 27 ? the answre is 26 that come 2*13 . here is the rimaining is the modulo , in our case 1
+// another example . 17%3 ? the solve is 3*5 =15 . what is remaining for 17 ? the answre 2 . that mean 2 the answre
+
+// -------------------Powre--------------
+2 ** 4; // 16
+
+//  -----------------------------------------NaN -> not a number--------------------
+// NaN is a numeric value that represents something that is .... not a number  and you cannot do math with it
+
+//  ------------------------------------------variables-----------------------------
+// we can store a value and give it  a name so that we can -refer back to it later - Use that value to do stuff -OR change it later one
+
+//  the stractuer to bulid a value ->              let somename =number; do not forget ; after value
+
+//  updating variables like let old = 5  and then old = old +5  that give us old = 10 . there is onther way to update -> old += 5 that mean old adding 5
+// and you can do subtraction  in the same way old -=5 old will subtracing
+// there is other ways to update like old++ that mean , the old will be 6  after enter ,ok try it and you will understanded
+// the same thing with old--
+
+// ---------------------------------------------------------const------------------------------------
+// const works just like let, except you cannot change the value
+// const hens =4; -> and you want update hens hens =10; -> ERROR
+
+// ---------------------------------------------------------- var --------------------------------------
+// the old variable keyword
+// before let & const , var was the only way of declaring variables . these days , there isn't really a reason to use it .
+
+// --------------------------------------------------booleans-----------------------------------------
+// true or false like old = true   , just not like c++  and also you can change it for numric like this old = 32 , it will changed
