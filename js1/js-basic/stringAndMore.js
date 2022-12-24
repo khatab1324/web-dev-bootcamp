b332e33cadbaab4a834d235a9f020a994c475604
kahtab1324 <k.s.fayyad.dev@gmail.com>
2022-12-24 18:28:41 +0200

    update

---
+++ js1/js-basic/stringAndMore.js
@@ -0,0 +1,4 @@
+// --------------------------------------------------string--------------------------------------------------------------
+// when you define a variable like a concetent , it need to be in notation like
+// let usrename ="khatab"; or username = 'Khatab Hero';
+// if you want to quote something it normal "he said that 'you are so stupeid'"
