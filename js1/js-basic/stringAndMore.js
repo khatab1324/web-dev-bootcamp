bac42e97951b0f1e6b333691227926a9b348de97
kahtab1324 <k.s.fayyad.dev@gmail.com>
2022-12-26 21:37:32 +0200

    update

--- js1/js-basic/stringAndMore.js
+++ js1/js-basic/stringAndMore.js
@@ -2,3 +2,22 @@
 // when you define a variable like a concetent , it need to be in notation like
 // let usrename ="khatab"; or username = 'Khatab Hero';
 // if you want to quote something it normal "he said that 'you are so stupeid'"
+
+// ------------------------------------- Indices & Length------------------
+// if you define integer like username
+// let username =khattab fayyad the first litter is k equal zreo
+// if i write username [5] the result will be a
+// that mean [0] -> k [1]-> h [2] -> a [3]->t [4]->t .....etc
+// if i write username .length that will give me the long of the name in our case username .length the answer 14
+
+// "lol"+"lol" the result is "lollol"
+// ok , if i put number with string like the 1+"hi" the result "1hi" and if you write typeof 1 it will give you number why ? because the there no way to add number without to be string
+
+// name the variable . toUpperCase() -> it make all the letter in like username uppercase
+// if make variable like let Hi ="I donot want to go " and make another variable let HiUpper = Hi.toUpperCase() the HiUpper will be I DONOT WANT TO GO
+//  toLowerCase same thing
+
+// nameVariable .trim() -> it cut the first space in the line like let username = "        cut me         "; and write username.trim()  it is will be "cut me"
+
+// let str = 'Hello, world!';   let index = str.indexOf('world');console.log(index); // 7In this example, the indexOf() method searches the string 'Hello, world!' for the value 'world'. It finds this value at position 7 (the first character has an index of 0), so it returns 7.
+// If the value is not found, the method returns -1.
