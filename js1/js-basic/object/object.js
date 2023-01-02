380c2eb93c8c53f4cd4f43b96750a1479b4047d4
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-01-02 18:44:48 +0200

    update

---
+++ js1/js-basic/object/object.js
@@ -0,0 +1,12 @@
+// to make Object
+const person = {
+  name: "John",
+  age: 30,
+  occupation: "software developer",
+  hobbies: ["coding", "reading", "gaming"],
+};
+//  to call to element in object
+console.log(person.name);
+//   name: 'John',  // key: 'name', value: 'John'
+// if you want make the call in [] remember to use '' and don't use the .
+console.log(person["occupation"]);
