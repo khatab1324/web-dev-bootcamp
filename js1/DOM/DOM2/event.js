633056a60309a4d0b1137c75c562011f696eb5c4
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-02-06 18:37:52 +0200

    update

---
+++ js1/DOM/DOM2/event.js
@@ -0,0 +1,6 @@
+// this better idea to but the onClick here
+const hoppButton = document.querySelector("#hopButton");
+hoppButton.onclick = function () {
+  console.log("your hoppy araived");
+  console.log("I will looking for your hoppy");
+};
