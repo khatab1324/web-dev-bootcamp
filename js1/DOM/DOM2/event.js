e9b7ae69d29e402922ae359200a57f82d8f49176
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-02-06 21:06:08 +0200

    update

--- js1/DOM/DOM2/event.js
+++ js1/DOM/DOM2/event.js
@@ -4,3 +4,10 @@ hoppButton.onclick = function () {
   console.log("your hoppy araived");
   console.log("I will looking for your hoppy");
 };
+function menuConect() {
+  console.log("menu");
+}
+// it should be inside function
+const Mconect = document.querySelector(".conect");
+Mconect.onmouseenter = menuConect;
+// onmouseenter just when you hover on the conect
