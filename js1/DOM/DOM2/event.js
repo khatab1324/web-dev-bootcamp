55aff5509e320c2f6966d512bd03a268321fba6f
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-02-07 21:03:12 +0200

    update

--- js1/DOM/DOM2/event.js
+++ js1/DOM/DOM2/event.js
@@ -11,3 +11,14 @@ function menuConect() {
 const Mconect = document.querySelector(".conect");
 Mconect.onmouseenter = menuConect;
 // onmouseenter just when you hover on the conect
+
+// -------------------------------------------------- addEventListener -------------------------------------
+// specify the event type and a callback to run
+// addEventListener('action','what happen')
+const signin = document.querySelector(".sign-in");
+signin.addEventListener("click", function () {
+  console.log("the user want signup");
+});
+// and we enter use addEventListener becuese it can show tow valiu at the same time
+
+// --------------------------------------------change color project --------------------------------------
