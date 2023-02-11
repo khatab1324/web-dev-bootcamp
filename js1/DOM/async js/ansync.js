ed11cc027b2f3394c6752b29bc4dce34a1cd8fab
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-02-11 14:31:32 +0200

    update

---
+++ js1/DOM/async js/ansync.js
@@ -0,0 +1,5 @@
+// ==================================== call stack =============================================================
+// you should knew stack the base of date structur in computer sincse
+// imagen you have alot of book , every book above on the other if you want add or remove you should do it
+// in this order
+// ok , what it is excuctly , it more than one function and every function called the other and realy on the onther function
