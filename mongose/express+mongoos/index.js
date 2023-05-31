f3dfd9b537accabe6aff13ec39392144c97c57fa
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-05-31 20:30:27 +0300

    make the first section of yelpcamp

--- mongose/express+mongoos/index.js
+++ mongose/express+mongoos/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
+const categories = ["fruit", "vegetable", "dairy"];
 const Product = require("./models/product");
 mongoose
   .connect("mongodb://127.0.0.1:27017/farmStand", {
@@ -32,9 +33,14 @@ app.get("/", (req, res) => {
   );
 });
 app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-
-  res.render("products/showProducts", { products });
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/showProducts", { products });
+  } else {
+    const products = await Product.find({});
+    res.render("products/showProducts", { products, categories });
+  }
 });
 app.get("/products/new", (req, res) => {
   res.render("products/newProduct");
@@ -54,7 +60,7 @@ app.get("/products/:id", async (req, res) => {
 app.get("/products/:id/edit", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  res.render("products/editProduct", { product });
+  res.render("products/editProduct", { product, categories });
 });
 //put come from method-override
 app.put("/products/:id", async (req, res) => {
@@ -67,7 +73,12 @@ app.put("/products/:id", async (req, res) => {
   await editProduct.save();
   res.redirect(`/products/${editProduct.id}`);
 });
-
+// now aplay the delete override in theProduct.ejs
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deleteProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
 app.listen(2000, () => {
   console.log("server satrt on port 2000");
 });
