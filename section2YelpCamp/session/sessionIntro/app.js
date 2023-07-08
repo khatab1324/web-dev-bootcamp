8353c5fc2348398cf32fb1ea112a377e3fe61657
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-07-08 21:25:53 +0300

    session

---
+++ section2YelpCamp/session/sessionIntro/app.js
@@ -0,0 +1,4 @@
+// ================session stored in server not in browser==============
+// cookies not secure //saved on browser//have limit of storing in browser
+// the browser send cookies have id for session in every req
+// .....The server can then take that ID and then go to the session data store and retrieve all relevant information
