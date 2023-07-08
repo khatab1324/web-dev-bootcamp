b35d0b888b45a9982f373fb9328e8ef00507b1c0
kahtab1324 <k.s.fayyad.dev@gmail.com>
2023-07-08 19:17:23 +0300

    same last one

--- section2YelpCamp/RouterAndCookies/Router/app.js
+++ section2YelpCamp/RouterAndCookies/Router/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
-
+const RouterHomePage = require("./routes/homepage");
+const RouterAdmin = require("./routes/admin");
+app.use("/homepage", RouterHomePage); //So what is this path here?Well, this is going to specify the prefix of all of the routes that we've predefined in that router.
+// We're just saying take all of those routes from that router and apply them to our app and prefix them with nothing, just with Slash.
+app.use("/admin", RouterAdmin);
 app.listen(3000, () => {
   console.log("Serving app on localhost:3000");
 });
